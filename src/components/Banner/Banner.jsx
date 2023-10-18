e683eb6aef2d644ca74b445cfdde8e93825075cb
kawsarkabir <devkawsarkabir@gmail.com>
2023-10-18 14:09:02 +0600

    client site all  static sections done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -29,46 +29,46 @@ const Banner = () => {
           <div className=" flex items-center  banner-section">
             <div className=" flex justify-evenly  max-w-screen-xl">
               <div className="lg:ml-44 ml-10">
-                <span className="text-blue-600 font-medium text-3xl mb-6">
+                <span className="text-[#FFBB38] font-medium text-3xl mb-6">
                   VR BOX 3D Glass
                 </span>
                 <h1 className="lg:text-5xl text-2xl font-bold my-3">
                   Explore Our Tech <br />
                   Collection Perfect <br /> Gadget
                 </h1>
-                <button className="btn btn-primary mt-5 px-12">Shop Now</button>
+                <button className="btn bg-[#FFBB38] border-none mt-5 px-12">Shop Now</button>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className=" flex items-center  banner-section">
+          <div className=" flex items-center  slider-2">
             <div className=" flex justify-evenly  max-w-screen-xl">
               <div className="lg:ml-44 ml-10">
-                <span className="text-blue-600 font-medium text-3xl mb-6">
+                <span className="text-[#FFBB38] font-medium text-3xl mb-6">
                   VR BOX 3D Glass
                 </span>
                 <h1 className="lg:text-5xl text-2xl font-bold my-3">
                   Explore Our Tech <br />
                   Collection Perfect <br /> Gadget
                 </h1>
-                <button className="btn btn-primary mt-5 px-12">Shop Now</button>
+                <button className="btn bg-[#FFBB38] border-none mt-5 px-12">Shop Now</button>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className=" flex items-center  banner-section">
+          <div className=" flex items-center  slider-3">
             <div className=" flex justify-evenly  max-w-screen-xl">
               <div className="lg:ml-44 ml-10">
-                <span className="text-blue-600 font-medium text-3xl mb-6">
+                <span className="text-[#FFBB38] font-medium text-3xl mb-6">
                   VR BOX 3D Glass
                 </span>
                 <h1 className="lg:text-5xl text-2xl font-bold my-3">
                   Explore Our Tech <br />
                   Collection Perfect <br /> Gadget
                 </h1>
-                <button className="btn btn-primary mt-5 px-12">Shop Now</button>
+                <button className="btn bg-[#FFBB38] border-none mt-5 px-12">Shop Now</button>
               </div>
             </div>
           </div>
--- src/components/FlashSale/FlashSale.jsx
+++ src/components/FlashSale/FlashSale.jsx
@@ -5,7 +5,7 @@ const FlashSale = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2  max-w-screen-xl mx-auto gap-6 my-20">
       <div className="campaing-left flex items-center">
-        <div className="ml-10">
+        <div className="ml-8">
           <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
             <div className="flex flex-col">
               <span className="countdown font-mono text-5xl">
@@ -39,16 +39,16 @@ const FlashSale = () => {
         </div>
       </div>
       <div className="campaing-right flex items-center">
-        <div className="ml-10 space-y-10">
+        <div className="ml-8 space-y-10">
         <p className="font-semibold">MOBILE APP VERSION</p>
         <h1 className="lg:text-4xl font-medium  ">
           Get Our <span className="text-[#FFBB38] underline">Mobile App</span> 
           <br />
           It’s Make easy for you life !
         </h1>
-        <div className="flex gap-x-6 items-center">
-          <img src="https://i.ibb.co/QMDntjH/apple-store.png" alt="" />
-          <img src="https://i.ibb.co/HPG4SC3/play-store.png" alt="" />
+        <div className="flex gap-x-4 items-center">
+          <img src="https://i.ibb.co/QMDntjH/apple-store.png" className="max-w-full" alt="" />
+          <img className="max-w-full" src="https://i.ibb.co/HPG4SC3/play-store.png" alt="" />
         </div>
         <img src="https://i.ibb.co/5hdFWs1/app-screen.png"  alt="" />
         </div>
