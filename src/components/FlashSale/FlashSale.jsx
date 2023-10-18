e7d9b88450d665222d6f285eaccdef81edcfee00
kawsarkabir <devkawsarkabir@gmail.com>
2023-10-18 12:51:18 +0600

    flash sale done

---
+++ src/components/FlashSale/FlashSale.jsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+import "./FlashSale.css";
+
+const FlashSale = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2  max-w-screen-xl mx-auto gap-6 my-10">
+      <div className="campaing-left flex items-center">
+        <div className="ml-10">
+          <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": 15 }}></span>
+              </span>
+              days
+            </div>
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": 10 }}></span>
+              </span>
+              hours
+            </div>
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": 24 }}></span>
+              </span>
+              min
+            </div>
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": 51 }}></span>
+              </span>
+              sec
+            </div>
+          </div>
+          <h1 className="font-semibold lg:text-4xl text-2xl my-5">
+            WOO! Flash Sale
+          </h1>
+          <Link className="underline mt-10">Shop Now</Link>
+        </div>
+      </div>
+      <div className="campaing-right flex items-center">
+        <div className="ml-10 space-y-10">
+        <p className="font-semibold">MOBILE APP VERSION</p>
+        <h1 className="lg:text-4xl font-semibold  ">
+          Get Our <span className="text-red-600 underline">Mobile App</span> 
+          <br />
+          It’s Make easy for you life !
+        </h1>
+        <div className="flex gap-x-6 items-center">
+          <img src="https://i.ibb.co/QMDntjH/apple-store.png" alt="" />
+          <img src="https://i.ibb.co/HPG4SC3/play-store.png" alt="" />
+        </div>
+        <img src="https://i.ibb.co/5hdFWs1/app-screen.png"  alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlashSale;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../components/Banner/Banner";
+import FlashSale from "../../components/FlashSale/FlashSale";
 import Services from "../../components/Services/Services";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Services></Services>
+      <FlashSale></FlashSale>
     </div>
   );
 };
